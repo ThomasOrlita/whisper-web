ed4fb97638707c33b7e0055b17cd674d6070c099
Thomas Orlita <info@thomasorlita.com>
2025-01-23 16:28:07 +0100

    Update Constants.ts

--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -1,7 +1,7 @@
 export default {
     SAMPLING_RATE: 16000,
     DEFAULT_AUDIO_URL: `https://huggingface.co/datasets/Xenova/transformers.js-docs/resolve/main/${
-        isMobileOrTablet ? "jfk" : "ted_60_16k"
+        "ted_60_16k"
     }.wav`,
     DEFAULT_MODEL: "Xenova/whisper-small",
     DEFAULT_SUBTASK: "transcribe",
